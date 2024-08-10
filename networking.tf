703ece5ac1eaf0d1bc751c30b13f5adbedbf2456
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-08-10 23:23:23 +0800

    Refactor(networking) migrate to private networking module

--- networking.tf
+++ networking.tf
@@ -1,16 +1,42 @@
-resource "aws_vpc" "tf_cloud" {
-  cidr_block = var.vpc_cidr
+# resource "aws_vpc" "tf_cloud" {
+#   cidr_block = var.vpc_cidr
 
-  tags = {
-    Name = "Terraform cloud"
-  }
+#   tags = {
+#     Name = "Terraform cloud"
+#   }
+# }
+
+# resource "aws_subnet" "tf_cloud" {
+#   cidr_block = var.subnet_cidr
+#   vpc_id     = aws_vpc.tf_cloud.id
+
+#   tags = {
+#     Name = "Terraform cloud"
+#   }
+# }
+
+moved {
+  from = aws_vpc.tf_cloud
+  to   = module.networking-tf-course.aws_vpc.this
+}
+moved {
+  from = aws_subnet.tf_cloud
+  to   = module.networking-tf-course.aws_subnet.this["subnet_1"]
 }
 
-resource "aws_subnet" "tf_cloud" {
-  cidr_block = var.subnet_cidr
-  vpc_id     = aws_vpc.tf_cloud.id
+module "networking-tf-course" {
+  source  = "app.terraform.io/weijie-test/networking-tf-course/aws"
+  version = "0.1.0"
+  # insert required variables here
+  vpc_config = {
+    cidr_block = var.vpc_cidr
+    name       = "terraform-cloud"
+  }
 
-  tags = {
-    Name = "Terraform cloud"
+  subnet_config = {
+    subnet_1 = {
+      cidr_block = var.subnet_cidr
+      az         = "ap-southeast-1a"
+    }
   }
 }
