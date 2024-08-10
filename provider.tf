027394c2df3db59371f92f3aafa812b1124597ff
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-08-10 22:45:07 +0800

    Feat add vpc

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  cloud {
+    organization = "weijie-test"
+    workspaces {
+      name = "intergration-with-terraform-cloud"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
