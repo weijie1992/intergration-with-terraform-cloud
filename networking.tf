ee36b65c312c19366d2bcd05f93ce493753793e1
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-08-10 22:59:04 +0800

    add subnet

--- networking.tf
+++ networking.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "tf_cloud" {
     Name = "Terraform cloud"
   }
 }
+
+resource "aws_subnet" "tf_cloud" {
+  cidr_block = var.subnet_cidr
+  vpc_id     = aws_vpc.tf_cloud.id
+
+  tags = {
+    Name = "Terraform cloud"
+  }
+}
